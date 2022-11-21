fbf012a0dcfdfaa50dedc326289af36ff8de361f
CWIN77 <cwstbp0426@gmail.com>
2022-11-21 18:26:59 +0900

    mongodb 연결 제거

--- src/lib/mongodb/connect.js
+++ src/lib/mongodb/connect.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
-const connection = {};
+export let isConnected = 0;
 
-const connectDB = async () => {
-  if (connection.isConnected) return;
+export const connectDB = async () => {
+  if (isConnected === 1) return;
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
-    connection.isConnected = db.connections[0].readyState;
+    isConnected = db.connections[0].readyState;
     console.log(`MongoDB Connected: ${db.connection.host}`);
   } catch (err) {
     console.log(err);
   }
 }
-
-export default connectDB
--- src/pages/mongo.tsx
+++ src/pages/mongo.tsx
@@ -1,18 +1,21 @@
 import styled from 'styled-components'
-import Goal from "../lib/mongodb/goal.model"
-import connectDB from "../lib/mongodb/connect"
-import { useEffect } from 'react'
+// import Goal from "../lib/mongodb/goal.model"
+// import { connectDB, isConnected } from "../lib/mongodb/connect"
+// import { getCookie, setCookie, deleteCookie } from 'cookies-next';
+// import { GetServerSideProps } from 'next'
 import Link from 'next/link';
+// import { useEffect } from 'react';
 
 export default function Mongo({ data }: any) {
   return (
     <Container>
       <Link href="/">이동</Link>
-      {
-        data.map((goal: any, key: any) => (
+      <div>테스트</div>
+      {/* {
+        data ? data.map((goal: any, key: any) => (
           <div key={key}><br />{goal._id} : {goal.text}</div>
-        ))
-      }
+        )) : null
+      } */}
     </Container>
   )
 }
@@ -22,17 +25,16 @@ const Container = styled.div`
   min-height:100vh;
 `
 
-export async function getServerSideProps() {
-  await connectDB();
-  const result = await Goal.find();
-  const goal = result.map((d) => {
-    const data = d.toObject();
-    data.createdAt = data.createdAt.toString();
-    data.updatedAt = data.updatedAt.toString();
-    data._id = data._id.toString();
-    return data;
-  });
-  return {
-    props: { data: goal }
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const result = await Goal.find();
+//   const goal = result.map((d) => {
+//     const data = d.toObject();
+//     data.createdAt = data.createdAt.toString();
+//     data.updatedAt = data.updatedAt.toString();
+//     data._id = data._id.toString();
+//     return data;
+//   });
+//   return {
+//     props: { data: goal }
+//   }
+// }
